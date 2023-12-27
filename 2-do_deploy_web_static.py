556ca8d445bf2c19ab280fffa2fdc613746da365
Umi252 <umisiguca25@gmail..com>
2023-12-27 04:14:20 -0800

    so many changes

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,5 +36,4 @@ def do_deploy(archive_path):
         run("ln -s {} /data/web_static/current".format(releases_path))
         print("New version deployed!")
         return True
-    except:
         return False
